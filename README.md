0cc2f4d4e8e316e3fc3114a34e3595402a8b7cd7
Gabriel Florit <gabrielflorit@gmail.com>
2017-09-06 17:07:34 -0400

    Initial barebones version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pointsToLineStrings
 Converts points to GeoJSON LineStrings
+
+## TODO
+- order by something (optional)
+- tell user if no stdin is present
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,47 @@
 #! /usr/bin/env node
 
-console.log('points to line stringss!')
+const dsv = require('d3-dsv')
+const getStdin = require('get-stdin')
+const _ = require('lodash')
+
+const argv = require('yargs')
+  .usage('Converts points to GeoJSON LineStrings.\nUsage: $0 <command>')
+
+  .describe('g', 'Grouping column')
+  .alias('g', 'group')
+
+  .help('h')
+  .alias('h', 'help')
+
+  .demand(['g'])
+  .argv
+
+getStdin()
+  .then(str => {
+
+    const { group } = argv
+
+    const features = _(dsv.csvParse(str))
+      .groupBy(group)
+      .map(value => ({
+        geometry: {
+          type: 'LineString',
+          coordinates: value.map(d => ([+d.lon, +d.lat])),
+        },
+        properties: {
+          [group]: value[0][group],
+        },
+      }))
+      .value()
+
+    const geojson = {
+      type: 'FeatureCollection',
+      features,
+    }
+
+    console.log(JSON.stringify(geojson, null, 2))
+
+  })
+  .catch(e => {
+    console.error(e)
+  })
